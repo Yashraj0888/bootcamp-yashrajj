6e357d03fec27200889344ec139788335dd60b1f
Yashraj Singh <121377022+Yashraj0888@users.noreply.github.com>
2025-01-10 16:36:09 +0530

    Update Header.tsx

--- product_dashboard/app/components/Header.tsx
+++ product_dashboard/app/components/Header.tsx
@@ -15,7 +15,7 @@ export default function Header() {
   return (
     <header className="bg-white shadow-md">
       <div className="container mx-auto px-4 py-4 flex justify-between items-center">
-        <Link href="/" className="text-2xl font-bold text-gray-800">
+        <Link href="/" className="text-2xl font-bold ">
           E-Kommerce
         </Link>
         <nav>
